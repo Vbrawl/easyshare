68d0189222be392beeb00c1f1222e7e0badce547
VBrawl <konstantosjim@gmail.com>
2024-11-16 02:30:24 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+BINARY_DIR="/usr/bin"
+
+usage() {
+  echo "$0 <options>"
+  echo
+  echo "options:"
+  echo "    -b BINARY_DIR    The directory in which to place the binary"
+  echo "    -h               Display this help menu"
+}
+
+install_binary() {
+  SOURCE="src/sendfile.sh"
+  DESTINATION="${BINARY_DIR}/sendfile"
+
+  echo "${SOURCE} -> ${DESTINATION}"
+  cp "$SOURCE" "$DESTINATION"
+  chmod +x "$DESTINATION"
+}
+
+
+while getopts ":hb:" arg
+do
+  case $arg in
+    b)
+      BINARY_DIR="$OPTARG"
+      ;;
+    h)
+      usage
+      exit
+      ;;
+    ?)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+install_binary
