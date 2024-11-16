002c5fed04e1df12e51beca0c0ec126ca3518c69
VBrawl <konstantosjim@gmail.com>
2024-11-16 03:04:55 +0200

    Added script to build .deb package

---
+++ build-deb.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+BUILD_DIRECTORY=$(mktemp -d)
+
+DEBIAN_DIRECTORY="${BUILD_DIRECTORY}/DEBIAN"
+BINARY_DIRECTORY="${BUILD_DIRECTORY}/usr/bin"
+
+mkdir -p "${DEBIAN_DIRECTORY}"
+mkdir -p "${BINARY_DIRECTORY}"
+
+cp "src/easyshare.sh" "${BINARY_DIRECTORY}/easyshare"
+chmod +x "${BINARY_DIRECTORY}/easyshare"
+
+cat > "${DEBIAN_DIRECTORY}/control" << EOF
+Package: easyshare
+Version: 0.0.1
+Section: web
+Priority: optional
+Architecture: all
+Maintainer: Jim Konstantos <konstantosjim@gmail.com>
+Description: Easily share files between computers and mobile devices through a QR code and wifi connectivity.
+Depends: qrencode (>= 4.1.1-1), python3 (>= 3.0), python-is-python3
+EOF
+
+
+dpkg-deb --root-owner-group --build "$BUILD_DIRECTORY" ./easyshare.deb
